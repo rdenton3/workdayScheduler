86701405c90846bbb82a6f2f4ce6f1f2a9891173
Rachel Denton <rdenton3@terpmail.umd.edu>
2021-12-26 17:11:22 -0500

    color blocked times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
+// Grab todays date and time
+var rightNow = moment().format("dddd, MMMM Do, YYYY");
+// display time and date at top of page
+$("#currentDay").text(rightNow)
 
-var currentTime = moment().format("hh:mm A")
-console.log(currentTime)
+// loop through timeblocks and assign as class past, present, or future
+var colorCode = function() {
+    // get current hour
+    var currentHr = moment().hour();
+    // compare currentHr to time in timeblock
+    $(".time-block").each(function() {
+        // get the time id in the current block and ocnvert it to a number
+        var getHr = parseInt($(this).attr("id"))
+        console.log(getHr)
+
+        // check time and apply appropriate class
+        if (getHr < currentHr) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+
+        else if (getHr === currentHr) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+    })
+}
+
+colorCode();
 
 var Time = moment().hour()
 console.log(Time)
